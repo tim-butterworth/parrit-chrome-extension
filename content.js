b70096ed0dcaed5d42621dc426665dc7aa589797
houron <pivotal.bolt@gmail.com>
2020-06-08 10:44:32 -0600

    Updates spinner to be r2d2 doing work

--- content.js
+++ content.js
@@ -302,7 +302,7 @@ const addSpinnerFunctionality = () => {
 			const container = document.querySelector(".project-page-container")
 			const spinnerDiv = document.createElement("div")
 
-			const imageUrl = chrome.runtime.getURL("parrit-spinner.gif");
+			const imageUrl = chrome.runtime.getURL("r2d2_search.gif");
 			const image = document.createElement("img");
 			image.setAttribute("src", imageUrl);
 			image.style.zIndex = "1001";
